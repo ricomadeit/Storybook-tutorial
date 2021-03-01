2ed12d3a93d56376d58018edb33f21b5f23f0061
Erik <R1COMADEIT@Eriks-MacBook-Pro.local>
2021-03-01 14:11:12 -0700

    Composite component and Data tutorial completed

---
+++ taskbox/src/components/TaskList.vue
@@ -0,0 +1,18 @@
+<!-- src/components/TaskList.vue -->
+
+<template>
+  <PureTaskList :tasks="tasks" v-on="$listeners" @archive-task="archiveTask"  @pin-task="pinTask"/>
+</template>
+
+<script>
+  import PureTaskList from './PureTaskList';
+  import { mapState, mapActions } from 'vuex';
+
+  export default {
+    components: { PureTaskList },
+
+    methods: mapActions(['archiveTask', 'pinTask']),
+
+    computed: mapState(['tasks']),
+  };
+</script>
